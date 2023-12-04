fd294173b32de386a577ee50c8ee3110f967bd76
AmberGraceSoftware <ambergracesoftware@gmail.com>
2023-12-04 15:16:23 -0700

    Document and implement Remote.Namespace

--- src/init.lua
+++ src/init.lua
@@ -31,23 +31,62 @@ export type Namespace = Types.Namespace
 ]=]
 local Remote = {}
 
+
+--[=[
+    @function Namespace
+    @within Remote
+    @param ... string
+    @return Remote
+    
+    Creates a copy of the Remote library that automatically prefixes key
+    arguments to all Remote library function calls.
+
+    For example:
+    ```lua
+    local wearOutfit = Remote.Function("AvatarEditor", "WearOutfit")
+    local equipItem = Remote.Event("AvatarEditor", "EquipItem")
+    local itemIds = Remote.State("AvatarEditor", "EquippedItemIDs", {} :: {string})
+    ```
+
+    In this example, the "AvatarEditor" key being used can converted to a
+    namespace:
+    ```lua
+    local AvatarRemotes = Remote.Namespace("AvatarEditor")
+    ```
+
+    Objects can then be created/accessed using the same API as the [Remote]()
+    library through the `AvatarRemotes` namespace:
+    ```lua
+    local wearOutfit = AvatarRemotes.Function("WearOutfit")
+    local equipItem = AvatarRemotes.Event("EquipItem")
+    local itemIds = AvatarRemotes.State("EquippedItemIDs", {} :: {string})
+    ```
+    
+]=]
 Remote.Namespace = function(...: string): Namespace
     local keys = table.pack(...)
     
     return table.freeze(setmetatable({}, {
-        __index = function(_self: any, methodName: string)
+        __index = function(_self: any, methodName: string): any
             local method = (Remote :: any)[methodName]
-            return function(...: any)
-                local postArgs = table.pack(...)
-                local newArgs = table.create(#keys + #postArgs)
-                for i = 1, keys.n do
-                    newArgs[i] = keys[i]
-                end
-                for i = 1, postArgs.n do
-                    newArgs[keys.n + i] = postArgs[i]
-                end
-                return method(table.unpack(newArgs))
+            if not method then
+                return nil
             end
+            if typeof(method) == "function" then
+                return function(...: any)
+                    local postArgs = table.pack(...)
+                    local newArgs = table.create(#keys + #postArgs)
+                    for i = 1, keys.n do
+                        newArgs[i] = keys[i]
+                    end
+                    for i = 1, postArgs.n do
+                        newArgs[keys.n + i] = postArgs[i]
+                    end
+                    return method(table.unpack(newArgs))
+                end
+            end
+
+            return method
         end,
     })) :: any
 end
